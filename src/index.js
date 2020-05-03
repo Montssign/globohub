609d06cbd1d5d3dbf127b2694b6cd182c1e3f264
Felipe D S Lima <felipe@monts.com.br>
2020-05-03 02:38:41 -0300

    adding redux to app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
 import './configs/ReactotronConfig';
 
+import { store, persistor } from '~/store';
 import Routes from './routes';
 
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#222222" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#222222" />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }
