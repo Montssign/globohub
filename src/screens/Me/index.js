480f22c0732277799b4787f5fc696a81ad7ecce9
Felipe D S Lima <felipe@monts.com.br>
2020-05-04 12:37:22 -0300

    Set Me Title

--- src/screens/Me/index.js
+++ src/screens/Me/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { setTitle } from '~/store/modules/title/actions';
 
 // import { Container } from './styles';
 
-export default function Me() {
+export default function Me({ navigation }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    navigation.addListener('focus', () => {
+      dispatch(setTitle('Meu Perfil'));
+    });
+  }, [dispatch, navigation]);
+
   return (
     <View>
       <Text>Eu</Text>
     </View>
   );
 }
+
+Me.propTypes = {
+  navigation: PropTypes.shape({
+    addListener: PropTypes.func,
+  }).isRequired,
+};
