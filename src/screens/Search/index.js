98abb91ceeb57769a55caca51a69d983bd25ac6d
Felipe D S Lima <felipe@monts.com.br>
2020-05-04 12:37:33 -0300

    Set Search Title

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { setTitle } from '~/store/modules/title/actions';
 
 // import { Container } from './styles';
 
-export default function Search() {
+export default function Search({ navigation }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    navigation.addListener('focus', () => {
+      dispatch(setTitle('Procurar na Globo Hub'));
+    });
+  }, [dispatch, navigation]);
+
   return (
     <View>
       <Text>Search</Text>
     </View>
   );
 }
+
+Search.propTypes = {
+  navigation: PropTypes.shape({
+    addListener: PropTypes.func,
+  }).isRequired,
+};
