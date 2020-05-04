d6fa54767f2160ef9545b795f5edd9dd3e7d1bb2
Felipe D S Lima <felipe@monts.com.br>
2020-05-04 12:37:17 -0300

    Set Friends Title

--- src/screens/Friends/index.js
+++ src/screens/Friends/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import Background from '~/components/Background';
+import { setTitle } from '~/store/modules/title/actions';
 
 // import { Container } from './styles';
 
-export default function Friends() {
+export default function Friends({ navigation }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    navigation.addListener('focus', () => {
+      dispatch(setTitle('Meus Amigos'));
+    });
+  }, [dispatch, navigation]);
+
   return (
     <View>
-      <Text>Friends</Text>
+      <Background>{/** resto */}</Background>
     </View>
   );
 }
+
+Friends.propTypes = {
+  navigation: PropTypes.shape({
+    addListener: PropTypes.func,
+  }).isRequired,
+};
