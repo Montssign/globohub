6c8557651de63661664f21bf952d1e73af9b7401
Felipe D S Lima <felipe@monts.com.br>
2020-05-04 16:53:57 -0300

    Input Component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import { Container, TextInput, Icon } from './styles';
 
-export default function Input({ icon, value, setValue }) {
+export default function Input({ icon, value, setValue, style, ...rest }) {
   return (
-    <View>
-      <Text>Input</Text>
-    </View>
+    <Container style={style}>
+      <TextInput
+        {...rest}
+        value={value}
+        hasIcon={!!icon}
+        defaultValue={value}
+        onChangeText={setValue}
+      />
+      {!!icon && <Icon name={icon} size={25} color="#ffffff50" />}
+    </Container>
   );
 }
 
@@ -16,8 +22,10 @@ Input.propTypes = {
   icon: PropTypes.string,
   value: PropTypes.string.isRequired,
   setValue: PropTypes.func.isRequired,
+  style: PropTypes.objectOf(PropTypes.any),
 };
 
 Input.defaultProps = {
   icon: '',
+  style: {},
 };
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components/native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  width: 100%;
+  padding: 16px;
+`;
+
+export const TextInput = styled.TextInput`
+  border-radius: 4px;
+  width: 100%;
+  background-color: #11111150;
+  height: 48px;
+  padding: 0 16px 0 ${props => (props.hasIcon ? 42 : 16)}px;
+`;
+
+export const Icon = styled(MaterialIcon)`
+  position: absolute;
+  top: 27px;
+  left: 27px;
+`;
